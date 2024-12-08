6906f2d5a35624b93e2838efa6b64ef40e8fb9ed
DeclanLacey <declan.lacey@hotmail.com>
2024-12-08 17:25:24 -0600

    Tests and implementation of handleToggle function

--- src/components/lightDarkModeToggle/LightDarkModeToggle.tsx
+++ src/components/lightDarkModeToggle/LightDarkModeToggle.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
-export default function LightDarkModeToggle() {
+function handleToggleClick(lightMode : Boolean, setLightMode : Function ) {
+  setLightMode(!lightMode)
+
+  if (lightMode) {
+    document.body.classList.add("dark-theme")
+  }else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function LightDarkModeToggle() {
+
+  const [lightMode, setLightMode] = useState<Boolean>(true)
 
   return (
-    <label>
-        <input type='Checkbox'></input>
-        <span></span>
-    </label>
+    <div>
+      <img src='./assets/icon-sun-light.svg' alt="Outline of sun"/>
+      <label>
+          <input onClick={() => handleToggleClick(lightMode, setLightMode)} type='Checkbox'></input>
+          <span></span>
+      </label>
+      <img src='./assets/icon-moon-light.svg' alt="Outline of moon"/>
+    </div>
+    
   )
 }
+
+export {LightDarkModeToggle, handleToggleClick}
--- src/test/LightDarkModeToggle.test.tsx
+++ src/test/LightDarkModeToggle.test.tsx
@@ -1,10 +1,59 @@
-import {render, screen} from "@testing-library/react"
-import LightDarkModeToggle from "../components/lightDarkModeToggle/LightDarkModeToggle"
+import {render, renderHook, screen} from "@testing-library/react"
+import {handleToggleClick, LightDarkModeToggle} from "../components/lightDarkModeToggle/LightDarkModeToggle"
+import { useState } from "react"
 
 describe("Testing LightDarkModeToggle component", () => {
     test("Checkbox should be visible", () => {
         render(<LightDarkModeToggle />)
         const checkbox = screen.queryByRole("checkbox")
         expect(checkbox).toBeVisible()
+    })
+
+    test("Sun image should be visible", () => {
+        render(<LightDarkModeToggle />)
+        const img = screen.queryByAltText("Outline of sun")
+        expect(img).toBeVisible()
+    })
+
+    test("Moon image should be visible", () => {
+        render(<LightDarkModeToggle />)
+        const img = screen.queryByAltText("Outline of moon")
+        expect(img).toBeVisible()
+    })
+
+    test("Should switch lightMode state to from true to false", () => {
+        const setLightMode = vi.fn()
+        const lightMode = true
+
+        handleToggleClick(lightMode, setLightMode)
+
+        expect(setLightMode).toHaveBeenCalledWith(false)
+    })
+
+    test("Should switch lightMode state from false to true", () => {
+      const setLightMode = vi.fn()
+      const lightMode = false
+      
+      handleToggleClick(lightMode, setLightMode)
+
+      expect(setLightMode).toHaveBeenCalledWith(true)
+    })
+
+    test("Should add the dark-theme class if lightMode is passed as true", () => {
+        const setLightMode = vi.fn()
+        const lightMode = true
+        
+        handleToggleClick(lightMode, setLightMode)
+
+        expect(document.body.classList.contains("dark-theme")).toBe(true)
+    }) 
+
+    test("Should remove the dark-theme class if lightMode is passed as false", () => {
+        const setLightMode = vi.fn()
+        const lightMode = false
+
+        handleToggleClick(lightMode, setLightMode)
+
+        expect(document.body.classList.contains("dark-theme")).toBe(false)
     })
 })
